1b9165c0c59fe9dc583329d5b25177f9d69aa4c7
gilberto martini <gilberto.madeira@outlook.com>
2021-01-31 15:17:36 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = new express();
+var carRoutes = require('./routes/car-routes');
+
+app. use('/cars', carRoutes);
+
+app.listen(5000,() => {
+	console.log('listening to port 5000');
+});
---
+++ routes/car-routes.js
@@ -0,0 +1,55 @@
+const crypto = require("crypto");
+var router = require('express').Router();
+var bodyParser = require('body-parser');
+
+var cars = [];
+
+router.use(bodyParser.json());
+router.use((req,res,next) =>{
+	var message = 'requisicao recebida em '+ new Date();
+	console.log(message);
+	next();
+});
+
+router.get('/:id?', (req,res) => {
+	var idParam = req.params.id;
+	if(idParam){
+		console.log('requisitando o carro com id ' + idParam);
+		selectedCar = cars.filter(c => c.id === idParam);
+		res.send(selectedCar);
+		return;
+	}
+	var nameFromQuery = req.query.name;
+	if(nameFromQuery){
+		var filteredCars = cars.filter(c => c.name.startsWith(nameFromQuery));
+		res.send(filteredCars);
+		return;
+	}
+
+	res.send(cars);
+});
+
+
+router.post('/', (req, res) => {
+	var postedCar = req.body;
+
+	postedCar.name = postedCar.name.toLowerCase();
+	var carAlreadyExists = cars.some(c => c.name === postedCar.name);
+
+	if(carAlreadyExists){
+		res.sendStatus(409);
+		return;
+	}
+	const id = crypto.randomBytes(16).toString("hex");
+	console.log(id); 
+
+	postedCar.id = id;
+	cars.push(postedCar);
+	res.sendStatus(201);
+});
+
+router.put('/:id', (req, res) => {
+	
+});
+
+module.exports = router;
